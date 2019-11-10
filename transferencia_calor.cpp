814ca3b184f3e394f3d986343966676fc05780be
Romeu I. L. Pires <rilpires@gmail.com>
2019-11-10 03:52:02 -0300

    improved transferencia_calor

--- transferencia_calor.cpp
+++ transferencia_calor.cpp
@@ -2,8 +2,11 @@
 #include <omp.h>
 #include <vector>
 #include <cstring>
+#include <cstdlib>
 
 using namespace std;
+
+enum {INCREASING , DECREASING};
 
 struct heat_point {
     int x;
@@ -27,100 +30,147 @@ float   ALPHA           = 0.2;
 size_t  MATRIX_SIZE     = 1024;
 float   INITIAL_TEMP    = 20;
 float   BORDER_TEMP     = 30;
-size_t  MAX_STEPS       = 512;
-vector<heat_point> heat_points = { heat_point(100,8,8) };
+size_t  MAX_STEPS       = 1024;
+size_t  BLOCK_SIZE      = 17;
+vector<heat_point> heat_points;
 
-void sor_step( float* mat , float* aux , 
+void sor_step( float** matrix_block , size_t current_step , size_t next_step , 
     size_t matrix_width , size_t matrix_height , 
-    size_t initial_y , size_t final_y , 
+    int initial_y , int final_y , 
+    short increasing_or_decreasing , size_t last_step ,
     vector<heat_point>& heat_points )
 {
-    float BETA = ((1.0-ALPHA)/4);
 
-    initial_y = max( (size_t)1,initial_y);
-    final_y = min( (size_t)(matrix_height-1) ,final_y);
-    //cout << initial_y << "," << final_y << endl;
+    //cout << "stesp: " << current_step << "->" << next_step << endl;
+    //cout << "last_step: " << last_step << endl;
+    
+    float BETA = ((1.0-ALPHA)/4);
+    float* current_matrix = matrix_block[current_step];
+    float* next_matrix = matrix_block[next_step];
+    
+    initial_y = max( 1,initial_y);
+    final_y = min( int(matrix_height-1) , final_y);
+    final_y = max(final_y,initial_y+1);
+    // cout << initial_y << "," << final_y << " [" << omp_get_thread_num() << "]" << endl;
 
     // Set temperature in each point a heat_point exists
     for( vector<heat_point>::iterator it = heat_points.begin() ; it != heat_points.end() ; it++ ){
         int y = it->y;
         if( y >= initial_y && y < final_y )
-            mat[it->x+y*matrix_height] = it->temperature;
+            current_matrix[it->x+y*matrix_height] = it->temperature;
     }
 
     for( int y = initial_y ; y < final_y ; y++ )
     for( int x = 1 ; x < matrix_width-1 ; x++ ){
-        aux[x+y*matrix_width] = mat[x+y*matrix_width]*ALPHA + BETA*(mat[x+(y+1)*matrix_width]+mat[x+(y-1)*matrix_width]+mat[x+1+y*matrix_width]+mat[x-1+y*matrix_width]);
+        next_matrix[x+y*matrix_width] = current_matrix[x+y*matrix_width]*ALPHA + BETA*(current_matrix[x+(y+1)*matrix_width]+current_matrix[x+(y-1)*matrix_width]+current_matrix[x+1+y*matrix_width]+current_matrix[x-1+y*matrix_width]);
     }
     
+    if( next_step < last_step ){
+        if( increasing_or_decreasing == INCREASING ){
+            {
+                sor_step( matrix_block , next_step , next_step + 1 , matrix_width , matrix_height , initial_y-1 , final_y+1 , INCREASING , last_step , heat_points );
+            }
+        } else if( increasing_or_decreasing == DECREASING ) {
+            {
+                sor_step( matrix_block , next_step , next_step + 1 , matrix_width , matrix_height , initial_y+1 , final_y-1 , DECREASING , last_step , heat_points );
+            }
+        }
+    }
+
 }
 
 int main( int argc , char** argv ){
     
     size_t num_threads = 1;
-    if( argc > 1 )
+    if( argc > 1 ){
         num_threads = atoi(argv[1]);
-    omp_set_num_threads(num_threads);
-    cout << "Numero de threads: " << num_threads << endl;
-
-    // Allocating memory to be used
-    float* mat  = new float[MATRIX_SIZE*MATRIX_SIZE];
-    float* aux  = new float[MATRIX_SIZE*MATRIX_SIZE];
-
-    // Setting initial temperatures around the middle
-    for( int y = 1 ; y < MATRIX_SIZE-1 ; y++ )
-    for( int x = 1 ; x < MATRIX_SIZE-1 ; x++ )
-        mat[x+y*MATRIX_SIZE] = INITIAL_TEMP;
-
-    // Setting initial temperature around the borders
-    for( int x = 0 ; x < MATRIX_SIZE ; x++ )
-        mat[x] = BORDER_TEMP;
-    for( int x = 0 ; x < MATRIX_SIZE ; x++ )
-        mat[x+(MATRIX_SIZE-1)*MATRIX_SIZE] = BORDER_TEMP;
-    for( int y = 0 ; y < MATRIX_SIZE ; y++ )
-        mat[y*MATRIX_SIZE] = BORDER_TEMP;
-    for( int y = 0 ; y < MATRIX_SIZE ; y++ )
-        mat[MATRIX_SIZE-1+y*MATRIX_SIZE] = BORDER_TEMP;
+        omp_set_num_threads(num_threads);
+    }
+    else
+    {
+        #pragma omp parallel
+        {
+            #pragma omp single
+            {
+                num_threads = omp_get_num_threads();
+            }
+        }
+    }
     
-    // aux gotta be the same as mat initially
-    memcpy( aux , mat , sizeof(float)*MATRIX_SIZE*MATRIX_SIZE );
+    cout << "Numero de threads: " << num_threads << endl;
+    
+    heat_points.push_back( heat_point(100,8,8) );
+    
+    // Allocating memory to be used
+    float**  mat_block = new float*[BLOCK_SIZE];
+    for( int b=0 ; b<BLOCK_SIZE ; b++ ){
+        mat_block[b] = new float[MATRIX_SIZE*MATRIX_SIZE];
+    }
+    
+    // Setting initial temperatures
+    for( int b=0 ; b<BLOCK_SIZE ; b++ ){
+        float* mat = mat_block[b];
+        // Setting initial temperatures around the middle
+        for( int y = 1 ; y < MATRIX_SIZE-1 ; y++ )
+        for( int x = 1 ; x < MATRIX_SIZE-1 ; x++ )
+            mat[x+y*MATRIX_SIZE] = INITIAL_TEMP;
+        // Setting initial temperature around the borders
+        for( int x = 0 ; x < MATRIX_SIZE ; x++ )
+            mat[x] = BORDER_TEMP;
+        for( int x = 0 ; x < MATRIX_SIZE ; x++ )
+            mat[x+(MATRIX_SIZE-1)*MATRIX_SIZE] = BORDER_TEMP;
+        for( int y = 0 ; y < MATRIX_SIZE ; y++ )
+            mat[y*MATRIX_SIZE] = BORDER_TEMP;
+        for( int y = 0 ; y < MATRIX_SIZE ; y++ )
+            mat[MATRIX_SIZE-1+y*MATRIX_SIZE] = BORDER_TEMP;
+    }
 
     // Running until MAX_STEPS of SOR
     double initial_t = omp_get_wtime();
+    
     #pragma omp parallel
     {
         #pragma omp master
         {
-            for( int step = 0 ; step < MAX_STEPS ; step++ ){
-                #pragma omp taskgroup
-                {
-                    for( int thread_id = 0 ; thread_id < num_threads ; thread_id++ ){
-                        #pragma omp task
-                        {    
-                            size_t initial_y = MATRIX_SIZE * (float(thread_id)/num_threads);
-                            size_t final_y   = MATRIX_SIZE * (float(thread_id+1)/num_threads);
-                            sor_step( mat , aux , MATRIX_SIZE , MATRIX_SIZE , initial_y , final_y , heat_points );
-                        }
+            for( int step = 0 ; step < MAX_STEPS ; step += BLOCK_SIZE-1 )
+            {
+                size_t max_block_steps = min( BLOCK_SIZE-1 , MAX_STEPS-step );
+
+                for( int t = 0 ; t < num_threads ; t++ ){
+                    #pragma omp task
+                    {
+                        int initial_y = MATRIX_SIZE * (float(t)/num_threads);
+                        int final_y   = MATRIX_SIZE * (float(t+1)/num_threads);
+                        size_t last_step = max_block_steps;
+                        sor_step( mat_block , 0 , 1 , MATRIX_SIZE , MATRIX_SIZE , initial_y , final_y , DECREASING , last_step , heat_points );
                     }
                 }
-                #pragma omp taskgroup
-                {
-                    for( int thread_id = 0 ; thread_id < num_threads ; thread_id++ ){
-                        #pragma omp task
-                        {    
-                            size_t initial_y = MATRIX_SIZE * (float(thread_id)/num_threads);
-                            size_t final_y   = MATRIX_SIZE * (float(thread_id+1)/num_threads);
-                            memcpy( mat , aux , sizeof(float)*MATRIX_SIZE*(final_y-initial_y) );
-                        }
+                #pragma omp taskwait
+
+                for( int t = 0 ; t < num_threads+1 ; t++ ){
+                    #pragma omp task
+                    {
+                        int initial_y = MATRIX_SIZE * ( float(t) / (num_threads+1) );
+                        int final_y = initial_y+1;
+                        size_t last_step = max_block_steps;
+                        sor_step( mat_block , 0 , 1 , MATRIX_SIZE , MATRIX_SIZE , initial_y , final_y , INCREASING , last_step , heat_points );
                     }
                 }
-                
+                #pragma omp taskwait
+
+                for( int t = 0 ; t < num_threads+1 ; t++ ){
+                    int initial_y = MATRIX_SIZE * (float(t)/num_threads);
+                    int final_y   = MATRIX_SIZE * (float(t+1)/num_threads);
+                    memcpy( (mat_block[0])+initial_y , (mat_block[max_block_steps])+initial_y , sizeof(float)*MATRIX_SIZE*(final_y-initial_y) );
+                }
+                #pragma omp taskwait
             }
         }
     }
     cout << "Tempo: " << omp_get_wtime() - initial_t << " segundos. "; 
-    cout << "Ponto(8,8) deve estar 63.6078: " << mat[7+MATRIX_SIZE*7] << endl;
+    cout << "Ponto(8,8) deve estar 63.9926: " << mat_block[0][7+MATRIX_SIZE*7] << endl;
 
-    delete[] mat;
-    delete[] aux;
+    for( int b = 0 ; b < BLOCK_SIZE ; b++ )
+        delete[] mat_block[b];
+    delete[] mat_block;
 }
